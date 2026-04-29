a5a580a16d6020e423ce7d0e3845ebf74981f22a
Rachid DAOUD <daoud.mohamed.rachid@gmail.com>
2026-04-29 12:41:07 +0200

    fix: add spring @repository

--- src/main/java/com/mahindra/tech/repositories/CityRepository.java
+++ src/main/java/com/mahindra/tech/repositories/CityRepository.java
@@ -2,6 +2,8 @@ package com.mahindra.tech.repositories;
 
 import com.mahindra.tech.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 }
--- src/main/java/com/mahindra/tech/repositories/CountryRepository.java
+++ src/main/java/com/mahindra/tech/repositories/CountryRepository.java
@@ -2,6 +2,8 @@ package com.mahindra.tech.repositories;
 
 import com.mahindra.tech.entities.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
 }
